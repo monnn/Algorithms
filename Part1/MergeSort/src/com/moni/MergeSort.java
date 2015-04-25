b00816f783da3b9333781d03c9b1c610b55f5fbf
Monica Shopova <monika.shopova@gmail.com>
2015-04-25 17:23:25 +0300

    some

--- Part1/MergeSort/src/com/moni/MergeSort.java
+++ Part1/MergeSort/src/com/moni/MergeSort.java
@@ -28,6 +28,7 @@ public class MergeSort {
         assert isSorted(a, low, high);
     }
 
+    //Top down Mergesort
     public void sort(Comparable[]a){
         Comparable[] aux = new Comparable[a.length];
         sort(a, aux, 0, a.length - 1);
@@ -44,14 +45,28 @@ public class MergeSort {
         merge(a, aux, low, mid, high);
     }
     
-        private static boolean isSorted(Comparable[] a, int lo, int hi) {
-            for (int i = lo + 1; i <= hi; i++)
-                if (a[i].compareTo(a[i-1]) < 0) {
-                    return false;
-                }
-            return true;
-        }
+    private static boolean isSorted(Comparable[] a, int lo, int hi) {
+        for (int i = lo + 1; i <= hi; i++)
+            if (a[i].compareTo(a[i-1]) < 0) {
+                return false;
+            }
+        return true;
+    }
 
+    //Bottom up Mergesort
+    public void buSort(Comparable[] a) {
+        int N = a.length;
+        Comparable[] aux = new Comparable[N];
+        for (int n = 1; n < N; n = n+n) {
+            for (int i = 0; i < N-n; i += n+n) {
+                int low = i;
+                int m  = i+n-1;
+                int high = Math.min(i+n+n-1, N-1);
+                merge(a, aux, low, m, high);
+            }
+        }
+        assert isSorted(a, 0, a.length);
+    }
     public static void main(String[] args){
         MergeSort m = new MergeSort();
         Integer[] array = {1,7,8,6,5,3,3,5,7,8,15,32,14,2,17,87,653,45};
@@ -59,6 +74,5 @@ public class MergeSort {
         for (int i = 0; i < array.length; i++) {
             System.out.println(array[i]);
         }
-
     }
 }
